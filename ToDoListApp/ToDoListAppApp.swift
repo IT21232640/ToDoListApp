5100edef9fead48f3310bbbf84f8274cb6d7581f
anuththara <it21232640@my.sliit.lk>
2024-11-26 11:08:55 +0530

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Anuththara Divarathna on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
